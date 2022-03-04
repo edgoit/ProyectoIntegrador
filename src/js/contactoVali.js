24f6e414a7c07670d1366a9674dfe9fe2d6dfd2e
ase <alejandrosaucedoenriquez@gmail.com>
2022-03-03 20:11:29 -0600

    Se realizaron cambios en contacto.html y se cambio todo el codigo de contactoVali.js

--- src/js/contactoVali.js
+++ src/js/contactoVali.js
@@ -1,68 +1,164 @@
-function validacionForm(nombre, email, telefono, mensaje) {
-    let valinombre = nombre.value;
-    let valiemail = email.value;
-    let valitelefono = telefono.value;
-    let valimensaje = mensaje.value;
-    let truemail = true;
+//Agregamos Datos desde contacto.html con ById a contactoVali.js
+const formulario = document.getElementById("form-registro-date");
+const btnReset = document.getElementById("resetBtn");
+const btnEnviar = document.querySelector("#enviar");
 
-    if (valiemail === "") {
-        email.classList.add("is-invalid");
-        truemail = false;
-        console.log(truemail);
-
-
-    }
-
-    if (valiemail = !valiemail.includes("@")) {
+// expresiones regulares para  validar nombre, email, telefono, y mensaje
+const ern = /[a-zA-Z]/;
+const er = /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
+const ert = /^([0-9]{5})+((-{1})*)+([0-9]{5})$/i;
+const erm = /^[a-zA-Z0-9?$@#()'!,+\-=_:.&€£*%\sÑñáéíóúÁÉÍÓÚ]+$/;
 
 
-        truemail = false;
-        console.log(truemail);
-
-        email.classList.add("is-invalid");
-
-    }
-
-
-
-
-
-
-    if (valitelefono === "") {
-        telefono.classList.add("is-invalid");
-
-    } if (valinombre === "") {
-        nombre.classList.add("is-invalid")
-
-    } if (valimensaje === "") {
-        mensaje.classList.add("is-invalid");
-    }
-
-
-    return truemail;
-}
-const formAddDate = document.getElementById("form-registro-date");
-
+//Declaramos variables que lleva el formulario 
 let nombre = document.getElementById("nombre");
 let email = document.getElementById("email");
 let telefono = document.getElementById("telefono");
 let mensaje = document.getElementById("mensaje");
 
-//let ancla = document.getElementById("wrapper");
+// se manda llamar la funcion evenListener
+eventListener();
 
 
-formAddDate.addEventListener("submit", (evento) => {
+// Funcion eventListener la cual valida los datos del formulario y el blur El evento blur es disparado cuando un elemento ha perdido su foco
+function eventListener() {
+    // cuando el formulario arranca
+    //document.addEventListener("DOMContentLoaded", iniciarForm);
+    nombre.addEventListener("blur", validacionFormNombre);
+    email.addEventListener("blur", validacionFormEmail);
+    telefono.addEventListener("blur", validacionFormTelefono);
+    mensaje.addEventListener("blur", validacionFormMensaje);
+    //enviar formulario
+    formulario.addEventListener("submit", enviar_formulario);
+}
 
-    evento.preventDefault();
-    let n = validacionForm(nombre, email, telefono, mensaje);
-    console.log(n);
-    let valinombre = nombre.value;
-    let valiemail = email.value;
-    let valitelefono = telefono.value;
-    let valimensaje = mensaje.value;
+// Funcion validacionFormNombre donde pasa el foco de verde si hay datos en el input nombre o rojo  si no hay datos 
+function validacionFormNombre(n) {
+    if (n.target.type === "text") {
+
+        if (ern.test(n.target.value)) {//test Prueba una coincidencia en una cadena. Devuelve true o false.
+            n.target.classList.remove("is-invalid");
+            n.target.classList.add("is-valid");
+        } else {
+            //mostrarError("El email no es valido");
+            n.target.classList.remove("is-valid");
+            n.target.classList.add("is-invalid");
+
+        }
+
+    }
+}
+
+
+// Funcion validacionFormEmail donde pasa el foco de verde si hay datos en el input email o rojo  si no hay datos 
+function validacionFormEmail(e) {
+    if (e.target.type === "email") {
+
+        if (er.test(e.target.value)) {
+            e.target.classList.remove("is-invalid");
+            e.target.classList.add("is-valid");
+        } else {
+            //mostrarError("El email no es valido");
+            e.target.classList.remove("is-valid");
+            e.target.classList.add("is-invalid");
+
+        }
+
+    }
+
+}
+
+
+
+// Funcion validacionFormTelefono donde pasa el foco de verde si hay datos en el input telefono o rojo  si no hay datos 
+function validacionFormTelefono(t) {
+    if (t.target.type === "text") {
+
+        if (ert.test(t.target.value)) {
+            t.target.classList.remove("is-invalid");
+            t.target.classList.add("is-valid");
+        } else {
+            //mostrarError("El email no es valido");
+            t.target.classList.remove("is-valid");
+            t.target.classList.add("is-invalid");
+
+        }
+
+    }
+}
+
+
+
+// Funcion validacionFormMensaje donde pasa el foco de verde si hay datos en el textarea mensaje o rojo  si no hay datos 
+function validacionFormMensaje(m) {
+    if (m.target.type === "textarea") {
+
+        if (m.target.value) {
+            m.target.classList.remove("is-invalid");
+            m.target.classList.add("is-valid");
+        } else {
+            //mostrarError("El email no es valido");
+            m.target.classList.remove("is-valid");
+            m.target.classList.add("is-invalid");
+
+        }
+
+    }
+}
+
+
+//Enviar el formulario
+function enviar_formulario(f) {
+    f.preventDefault();
+    //Mostar el spinner
+    const spinner = document.querySelector("#spinner");
+    spinner.style.display = "flex";
+
+    // Despues de tres segundos ocultar el spinner
+    setTimeout(() => {
+        spinner.style.display = "none";
+       let valorNombre = ern.test(nombre.value);
+       let valorTelefono = ert.test(telefono.value);
+       let valorMensaje = erm.test(mensaje.value);
+
+        if(valorNombre == "" || valorTelefono == "" || valorMensaje == ""){
+       console.log(valorNombre);
+        // mensaje que dice que la informacion de contacto tiene errores
+        Swal.fire({
+            title: 'Error',
+            text: 'La informacion de contacto tiene errores',
+            icon: 'Danger',
+            confirmButtonText: 'continuar',
+            confirmButtonColor: 'black'
+        });
+    }else{
+            // mensaje que dice que el formulario se envio correctamente 
+        Swal.fire({
+            title: 'Exito',
+            text: 'Se guardo tu informacion de contacto',
+            icon: 'success',
+            confirmButtonText: 'continuar',
+            confirmButtonColor: 'black'
+        });
+
+    }
+
+    }, 3000);
+
+}
+
+
+//Reinicia el formulario 
+formulario.addEventListener("reset", (e) => {
+    e.preventDefault();
+    nombre.value = "";
+    email.value = "";
+    telefono.value = "";
+    mensaje.value = "";
+});
 
 
 
 
 
-})
+
